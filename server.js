c44c076208596c2a6e82b5cd0530a86fafaf9870
Rahul Sah <rahulsah.eng@gmail.com>
2018-05-15 22:28:49 +0530

    server.js changed

---
+++ server.js
@@ -0,0 +1,51 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app=express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    var log=`${now}:${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n');
+    next();
+});
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs');
+// });
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+app.get('/',(req,res)=>{
+    //res.send('<h1>Hello Express</h1>');
+    // res.send({
+    //     name:'rahul',
+    //     likes:['Biking','Cities']
+    // }); 
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMessage:'Welcome to my website'
+        
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page'
+       
+    });
+    
+});
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:'Unable to handle request'   
+    });
+    
+});
+app.listen(3000);
